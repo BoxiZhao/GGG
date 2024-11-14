53567959526cb833c7acd791bc867391e9f03c0c
Vagus <135856162+BoxiZhao@users.noreply.github.com>
2024-11-14 18:09:22 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -7,27 +7,18 @@ from flask import Flask, request, render_template, g, redirect, Response, sessio
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-app.secret_key = '670296'
+app.secret_key = 'bz2540'
 
-DATABASEURI = "postgresql://postgres:670296@localhost/postgres"
+DATABASEURI = "postgresql://bz2540:bz2540@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/w4111"
 engine = create_engine(DATABASEURI)
 
 with open('Create_Table.txt', 'r', encoding="utf-8") as f:
 	createTableSQL = f.read()
-	#不知道是不是版本冲突，直接engine不上
-	with engine.connect() as connection:
-    connection.execute(createTableSQL)
-
+	engine.execute(createTableSQL)
 
 with open('Initial_Data.txt', 'r', encoding="utf-8") as f:
 	initialDataSQL = f.read()
-	with engine.connect() as connection:
-    connection.execute(initialDataSQL)
-
-# they should be read out from the database
-userRange = range(1, 77)
-adminRange = range(1, 13)
-
+	engine.execute(initialDataSQL)
 
 @app.before_request
 def before_request():
@@ -37,7 +28,6 @@ def before_request():
 		print("uh oh, problem connecting to database")
 		import traceback; traceback.print_exc()
 		g.conn = None
-
 
 @app.teardown_request
 def teardown_request(exception):
@@ -58,15 +48,17 @@ def index():
 
 	cursor = g.conn.execute("""
 		SELECT E.event_Name, E.event_Description, U.user_Name, E.event_ID
-		FROM Event_Aggregation E, User_List U
+		FROM Event_Aggregation E, User_List U, Request_List R
 		WHERE E.organizer_ID = U.user_ID
+		AND E.request_ID = R.request_ID
+		AND R.request_Approval = 1
 		""")
 	eventList = []
 	for result in cursor:
-		eventList.append([result[0], result[1], result[2], '\\eventdetail\\'+str(result[3])])
+		eventList.append([result[0], result[1], result[2], '/eventdetail/'+str(result[3])])
 	cursor.close()
 
-	context = dict(data = eventList, login = session.get('userID'))
+	context = dict(data = eventList, userID = session.get('userID'), adminID = session.get('adminID'))
 
 	return render_template("index.html", **context)
 
@@ -75,8 +67,23 @@ def index():
 def login(): 
 	if request.method == 'POST':
 		userID = eval(request.form['userID'])
-		if userID in userRange:
+		adminID = eval(request.form['adminID'])
+		cursor = g.conn.execute("""
+			SELECT U.user_ID
+			FROM User_List U
+			""")
+		userRange = cursor.all()
+		cursor.close()
+		cursor = g.conn.execute("""
+			SELECT A.admin_ID
+			FROM Admin_List A
+			""")
+		adminRange = cursor.all()
+		cursor.close()
+		if (userID,) in userRange:
 			session['userID'] = userID
+			if (adminID,) in adminRange:
+				session['adminID'] = adminID
 			return redirect('/')
 		else:
 			print(userID)
@@ -93,15 +100,28 @@ def logout():
 @app.route('/addevent', methods=['POST', 'GET'])
 def add_event():
 	if request.method == 'POST':
-		cmd = 'INSERT INTO Event_Aggregation(event_Name, event_Description, participant_Limit, organizer_ID) VALUES (:event_Name, :event_Description, :participant_Limit, :organizer_ID)';
+		cursor = g.conn.execute("""SELECT COUNT(*) FROM Event_Aggregation""")
+		eventCounter = cursor.all()[0][0] + 1
+		cursor.close()
+
+		cmd = 'INSERT INTO Request_List(request_ID, request_Comment, request_Approval) VALUES (:request_ID, :request_Comment, 0)'
+		g.conn.execute(
+			text(cmd),
+			request_ID = eventCounter,
+			request_Comment = request.form['event_Name']
+		)
+
+		cmd = 'INSERT INTO Event_Aggregation(event_Name, event_Description, participant_Limit, organizer_ID, request_ID) VALUES (:event_Name, :event_Description, :participant_Limit, :organizer_ID, :request_ID)'
 		g.conn.execute(
 			text(cmd),
 			event_Name = request.form['event_Name'],
 			event_Description = request.form['event_Description'],
 			participant_Limit = request.form['participant_Limit'],
-			organizer_ID = session['userID']
+			organizer_ID = session['userID'],
+			request_ID = eventCounter
 		)
 		return redirect('/')
+
 	if session.get('userID'):
 		return render_template("addevent.html")
 	return redirect("/login")
@@ -109,12 +129,121 @@ def add_event():
 
 @app.route('/eventdetail/<int:eventID>/', methods = ['GET'])
 def event_detail(eventID):
-	context = dict(eventID = eventID)
+	session['eventID'] = eventID
+
+	cursor = g.conn.execute("""
+		SELECT E.event_Name, E.event_Description, L.location_Name, W.time_Start, W.time_End, E.organizer_ID
+		FROM Event_Aggregation E
+		LEFT JOIN Location_When LW ON E.location_when_ID = LW.location_when_ID
+		LEFT JOIN Location_List L ON LW.location_ID = L.location_ID
+		LEFT JOIN When_List W ON LW.when_ID = W.when_ID
+		WHERE E.event_ID = %d
+	""" % eventID)
+	eventDetail = cursor.fetchone()
+	cursor.close()
+
+	userID = session.get('userID')
+	isOrganizer = userID == eventDetail[5]
+	cursor = g.conn.execute("""SELECT * FROM User_Event""")
+	registered = (userID, eventID) in cursor.all()
+	cursor.close()
+
+	cursor = g.conn.execute("""
+		SELECT *
+		FROM Notification_List N
+		WHERE N.event_ID = %d
+	""" % eventID)
+	notificationList = []
+	for result in cursor:
+		notificationList.append([result[1], result[2], result[3]])
+	cursor.close()
+
+	context = dict(eventDetail=eventDetail, notificationList=notificationList, registered = registered, userID = userID, isOrganizer = isOrganizer)
 	return render_template("eventdetail.html", **context)
+
 
 @app.route('/register', methods = ['POST'])
 def register():
-	return redirect('/')
+	cmd = 'INSERT INTO User_Event VALUES (:user_ID, :event_ID)'
+	g.conn.execute(
+		text(cmd),
+		user_ID = session['userID'],
+		event_ID = session['eventID']
+	)
+	return redirect('/eventdetail/%d/' % session['eventID'])
+
+
+@app.route('/requests', methods = ['GET'])
+def requests():
+
+	result_dict = {-1:'Rejected', 0:'Under Review', 1:'Approved'}
+	cursor = g.conn.execute("""SELECT * FROM Request_List""")
+	requestList = []
+	for result in cursor:
+		requestList.append([result[0], result[1], result_dict[result[2]], '/requestdetail/'+str(result[0])])
+	cursor.close()
+
+	context = dict(data = requestList)
+
+	return render_template("requests.html", **context)
+
+
+@app.route('/requestdetail/<int:requestID>/', methods = ['GET'])
+def request_detail(requestID):
+	session['requestID'] = requestID
+	cursor = g.conn.execute("""
+		SELECT E.event_Name, E.event_Description, U.user_Name, E.event_ID
+		FROM Event_Aggregation E, User_List U
+		WHERE E.organizer_ID = U.user_ID AND
+			E.request_ID = %d
+		""" % requestID)
+	eventList = []
+	for result in cursor:
+		eventList.append([result[0], result[1], result[2], '/eventdetail/'+str(result[3])])
+	cursor.close()
+
+	cursor = g.conn.execute("""
+		SELECT R.request_Approval
+		FROM Request_List R
+		WHERE R.request_ID = %d
+		""" % requestID)
+	reviewed = cursor.all()[0][0]
+	cursor.close()
+
+	context = dict(data = eventList, reviewed = reviewed, requestID = requestID)
+
+	return render_template("requestdetail.html", **context)
+
+
+@app.route('/review', methods = ['POST'])
+def review():
+	cmd = 'UPDATE Request_List SET request_Approval = :result WHERE request_ID = :requestID'
+	g.conn.execute(
+		text(cmd),
+		result = eval(request.form['feedback']),
+		requestID = session['requestID']
+	)
+	return redirect('/requests')
+
+
+@app.route('/addnotification', methods=['POST', 'GET'])
+def add_notification():
+    if request.method == 'POST':
+
+        cmd = """
+            INSERT INTO Notification_List(notification_Title, notification_Content, post_Time, event_ID)
+            VALUES (:notification_Title, :notification_Content, NOW(), :event_ID)
+        """
+        g.conn.execute(
+            text(cmd),
+            notification_Title=request.form['notification_Title'],
+            notification_Content=request.form['notification_Content'],
+            event_ID=session['eventID']
+        )
+        return redirect('/eventdetail/' + str(session['eventID']))
+
+    return render_template("addnotification.html")
+
 
 if __name__ == "__main__":
 	import click
@@ -123,7 +252,7 @@ if __name__ == "__main__":
 	@click.option('--debug', is_flag=True)
 	@click.option('--threaded', is_flag=True)
 	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8224, type=int)
+	@click.argument('PORT', default=8111, type=int)
 
 	def run(debug, threaded, host, port):
 		HOST, PORT = host, port
